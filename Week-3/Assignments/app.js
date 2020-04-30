de7d7972c8be8b845e4c55d8092e2348e9925f91
Yen-Chen Kuo <yenchenkuo9331@gmail.com>
2020-04-30 12:52:07 +0800

    Assignment-1 Create Server Using Express

---
+++ Week-3/Assignments/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => res.send('<h1>Hello, My Server!</h1>'));
+app.listen(port, () => console.log('The applicaiton is running on localhost:3000'));
