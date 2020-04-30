88f8830d3e07c54c26157b3a6d0ab913e6474d92
Yen-Chen Kuo <yenchenkuo9331@gmail.com>
2020-04-30 14:45:15 +0800

    Serve static files in public folder to root

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const mainRoutes = require('./routes');
+app.use(express.static('public'))
 
+const mainRoutes = require('./routes');
 app.use(mainRoutes);
+
 app.listen(port, () => console.log('The applicaiton is running on localhost:3000'));
