eab20e2e031cb0e9417033e5c8d8140b5df15647
Yen-Chen Kuo <yenchenkuo9331@gmail.com>
2020-05-02 18:53:10 +0800

    Week3 - Time Complexity Test

--- Week-3/Assignment-5/assignment-5.js
+++ Week-3/Assignment-5/assignment-5.js
@@ -26,11 +26,12 @@ function twoSumByHash(nums, target) {
             numsMap.set(nums[i], i);
         }
     }
+    //Result Not Found
     return false;
 }
 
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSumByHash([2, 7, 11, 15], 9));
+//console.log(twoSum([2, 7, 11, 15], 9));
+//console.log(twoSumByHash([2, 7, 11, 15], 9));
 
 
 /*
@@ -41,3 +42,44 @@ Should returns:
 Because:
 nums[0]+nums[1] is 9
 */
+
+const {performance} = require('perf_hooks');
+function testArray(length) {
+    return arr = Array.from({length: length}, () => Math.floor(Math.random() * length));
+}
+
+function functionTimeRecord(testArr, target, testFunction) {
+    //node.js performance class
+    console.log(testFunction);
+    let startTime = performance.now();
+    console.log(testFunction(testArr, testArr[target[0]]+testArr[target[1]]));
+    let endTime = performance.now();
+    let duration = endTime - startTime;
+    console.log("duration: " + duration + "ms");
+}
+
+function twoSumTest(testArr = null, length=0, target) {
+    if (testArr === null) {
+        testArr = testArray(length);
+    }
+    console.log("Test Array Length: " + testArr.length);
+    console.log("Target: [" + target + "]");
+    functionTimeRecord(testArr, target, twoSum);
+    functionTimeRecord(testArr, target, twoSumByHash);
+    console.log("============================")
+}
+
+twoSumTest([2, 7, 11, 15], 0, [0, 1]);
+twoSumTest([2, 7, 11, 15], 0, [0, 2]);
+twoSumTest([2, 7, 11, 15], 0, [0, 3]);
+
+twoSumTest([2, 7, 11, 15], 0, [1, 2]);
+twoSumTest([2, 7, 11, 15], 0, [1, 3]);
+
+twoSumTest([2, 7, 11, 15], 0, [2, 3]);
+
+// twoSumTest(null, 3000, [0, 2999]);
+// twoSumTest(null, 3000, [2998, 2999]);
+// twoSumTest(null, 30000, [0, 29999]);
+// twoSumTest(null, 300000, [0, 299999]);
+// twoSumTest(null, 3000000, [0, 2999999]);
