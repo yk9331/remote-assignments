e790b4d9f019ca892f7a217401451dcafc7bde09
Yen-Chen Kuo <yenchenkuo9331@gmail.com>
2020-05-05 01:30:26 +0800

    Connect to MySQL

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const mysql = require('mysql');
 
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'assignment'
+});
+
+db.connect((err) => {
+    if (err) { throw err; }
+    console.log("MySQL Connected.");
+})
 
 const app = express();
 const port = 3000;
@@ -8,4 +20,4 @@ app.get('/', (req,res) => {
     res.send('Get Root');
 });
 
-app.listen(port, () => console.log(" The application is running on localhost:3000"));
+app.listen(port, () => console.log("The application is running on localhost:3000"));
