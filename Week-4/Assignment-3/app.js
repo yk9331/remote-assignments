c9ce7042c1fa17016bc892e6cbd905c4e5ff919a
Yen-Chen Kuo <yenchenkuo9331@gmail.com>
2020-05-07 00:22:27 +0800

    Debug signup API

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -15,9 +15,9 @@ db.connect((err) => {
     console.log("MySQL Connected.");
 })
 
-async function ckeckEmailValidation(email) {
+function ckeckEmailValidation(email) {
     let sql = `SELECT email FROM user WHERE email = "${email}";`;
-    let query = await db.query(sql, (err, result) => {
+    let query = db.query(sql, (err, result) => {
         if (err) throw err;
         console.log(result.length);
         if (result.length == 0) {
@@ -38,27 +38,26 @@ app.get('/', (req,res) => {
     res.redirect('/index.html');
 });
 
-app.post('/signup', (req, res) => {
-    if (ckeckEmailValidation(req.body.email)) {
+app.post('/signup', async (req, res) => {
+    if (await ckeckEmailValidation(req.body.email)) {
         const user = {
             username: req.body.username,
             email: req.body.email,
             password: req.body.password
         };
-        console.log(user);
         let sql = `INSERT INTO user SET ?`;
         let query = db.query(sql, user, (err, result) => {
             if (err) throw err;
             res.redirect('member.html');
         });
     } else {
-        console.log(req.body.email);
-        res.send('This email already exist, try to log in');
+        //res.send('This email already exist, try to log in');
+        res.redirect('/');
     }
 })
 
 app.post('/signin', (req, res) => {
-    res.send('sign in');
+    
 })
 
 app.listen(port, () => console.log("The application is running on localhost:3000"));
