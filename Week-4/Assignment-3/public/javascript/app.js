826e95b92dead13da8f31dd961294f201e1cae16
Yen-Chen Kuo <yenchenkuo9331@gmail.com>
2020-05-07 00:13:11 +0800

    Client-side input validation

--- Week-4/Assignment-3/public/javascript/app.js
+++ Week-4/Assignment-3/public/javascript/app.js
@@ -1,5 +1,11 @@
-const signUpForm = document.getElementById('sign-up-form');
-const signInForm = document.getElementById('sign-in-form');
+const signUpForm = document.querySelector('#sign-up-form');
+const signUpUsername = document.querySelector('#sign-up-username');
+const signUpEmail = document.querySelector('#sign-up-email');
+const signUpPassword = document.querySelector('#sign-up-password');
+
+const signInForm = document.querySelector('#sign-in-form');
+const signInEmail = document.querySelector('#sign-in-email');
+const signInPassword = document.querySelector('#sign-in-password');
 
 function showSignUpForm(){
     signUpForm.style.display = 'flex';
@@ -10,10 +16,30 @@ function showSignInForm(){
     signInForm.style.display = 'flex';
 }
 
+function checkSignUpFormValidation() {
+    if (signUpUsername.validity.valid && signUpEmail.validity.valid && signUpPassword.validity.valid) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkSignInFormValidation() {
+    if (signInEmail.validity.valid && signInPassword.validity.valid) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
 signUpForm.addEventListener('submit', (e) => {
-     
+    if (!checkSignUpFormValidation) {
+        e.preventDefault();
+     }
 });
 
 signInForm.addEventListener('submit', (e) => {
-    
+    if (!checkSignInFormValidation) {
+        e.preventDefault();
+     }
 })
